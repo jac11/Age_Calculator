7ea82ba3a91a0edd9e37accbd7261f51deb154d0
jac11 <rootx1982@gmail.com>
2025-01-08 07:28:11 -0500

    Update Age_Calculator.c

--- Age_Calculator.c
+++ Age_Calculator.c
@@ -1,116 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 
-
-int Bday    , BMonths  ,  Byears   ;
-int Cday    , CMonths  ,  Cyears   ;
-int resultday  , resultMonths ,  resultyears ;
+int Bday, BMonths, Byears;
+int Cday, CMonths, Cyears;
+int resultday, resultMonths, resultyears;
 
 int info(void);
-int Opretion(int , int ,int , int ,int ,int) ;
+int Opretion(int, int, int, int, int, int);
 
+int info(void) {
+    printf("[*] Birthday Value (dd MM YYYY): ");
+    scanf("%d %d %d", &Bday, &BMonths, &Byears);
+    
+    if (Bday > 31 || BMonths > 12 || Bday < 1 || BMonths < 1) {
+        printf("[*] Birthday Value (dd MM YYYY): Format not Correct\n");
+        return info();
+    }
 
-int info(void){
-          
-        printf ("[*]Birthday Value dd/MM/YYYY ............... |  ");
+    printf("\nDay - Month - Year\n");
+    printf("%2d %6d %9d\n\n", Bday, BMonths, Byears);
 
-        scanf("%d"  , & Bday)   ;  
-        scanf("%d"  , & BMonths);
-        scanf("%d"  , & Byears );
-        if (Bday  >= 32 || BMonths >= 13 ){
-                
-                printf("[*]Birthday Value dd/MM/YYYY ............... |  Format not Correct \n");
-                return info();
-        }         
-        printf ("\n\n day  -  Months - years \n\n");
+    printf("[*] Current Date Value (dd MM YYYY): ");
+    scanf("%d %d %d", &Cday, &CMonths, &Cyears);
 
-        printf("%4d   " , Bday)   ;
-        printf("%6d   " , BMonths);
-        printf("%5d   " , Byears) ;
-        printf("\n\n");
+    if (Cday > 31 || CMonths > 12 || Cday < 1 || CMonths < 1) {
+        printf("[*] Current Date Value (dd MM YYYY): Format not Correct\n");
+        return info();
+    }
 
-        printf ("[*]Current Data Value  dd/MM/YYYY ........... |  ");
+    printf("\nDay - Month - Year\n");
+    printf("%2d %6d %9d\n", Cday, CMonths, Cyears);
 
-        scanf("%d" ,  & Cday)   ; 
-        scanf("%d" ,  & CMonths);
-        scanf("%d" ,  & Cyears ) ;
-        if (Cday >= 32 || CMonths >=13){
-             
-                printf("[*]Current Data Value  dd/MM/YYYY ........... |  Format not Correct  \n");
-                return info();
-        }        
-        printf ("\n\n day  -  Months - years \n\n");
-        printf("%4d    " , Cday)   ;
-        printf("%6d    " , CMonths);
-        printf("%4d    " , Cyears) ;
-
-        return (0);
-}
-int Opretion(int Bday,int BMonths,int Byears,int Cday,int CMonths,int Cyears){
-
-        if (Cday >= Bday ){            
-            resultday = Cday - Bday ;
-        }
-        else {
-            if(Cday < Bday){
-                if (Cday == 31){                   
-                    Cday  = Cday + 31 ;
-                }else{
-                   Cday  = Cday + 30 ;
-                }
-                CMonths  = CMonths - 1 ;
-                resultday = Cday - Bday ;
-                if (CMonths == 0 ){
-                    CMonths = CMonths +12;
-                    Cyears -1;
-                }
-            }               
-        }
-        
-        if (CMonths >= BMonths){
-                 resultMonths = CMonths - BMonths ;
-        }
-        else{
-          if (CMonths == 0 || CMonths < BMonths ){
-               CMonths = CMonths +12;
-               Cyears  = Cyears -1 ;
-               resultMonths = CMonths - BMonths ;      
-          }
-        }
-        
-        if( Cyears > Byears  ||Cyears == Byears ){
-             resultyears =  Cyears - Byears;
-        }else{
-            if (Cyears < Byears){
-                 resultyears =  Cyears - Byears;
-            }   
-        }
-            return 0 ;
-}
-int main (){
-
-        printf("\n");
-        printf("===================================\n");    
-        printf("    Welcome To Days Calculator    \n" ); 
-        printf("===================================\n\n");
-        sleep(1);
-
-        info();
-        Opretion(Bday,BMonths,Byears,Cday,CMonths,Cyears);
-        
-        printf("\n==================================\n");
-        printf("            the Resulet              \n");
-        printf("====================================");
-
-        printf ("\n\n days  -  Months - years \n\n");
-        printf ("%4d   ",resultday)   ;
-        printf("%6d    " ,resultMonths);
-        printf("%4d    " ,resultyears) ;
-
+    return 0;
 }
 
+int Opretion(int Bday, int BMonths, int Byears, int Cday, int CMonths, int Cyears) {
+    if (Cday < Bday) {
+        Cday += 30;  // Approximation for month length
+        CMonths -= 1;
+    }
+    resultday = Cday - Bday;
 
+    if (CMonths < BMonths) {
+        CMonths += 12;
+        Cyears -= 1;
+    }
+    resultMonths = CMonths - BMonths;
 
+    resultyears = Cyears - Byears;
 
+    return 0;
+}
+
+int main() {
+    printf("\n===================================\n");
+    printf("    Welcome To Days Calculator    \n");
+    printf("===================================\n\n");
+
+    info();
+    Opretion(Bday, BMonths, Byears, Cday, CMonths, Cyears);
+
+    printf("\n==================================\n");
+    printf("            Result              \n");
+    printf("==================================\n");
+
+    printf("\nDays - Months - Years\n");
+    printf("%4d %6d %6d\n", resultday, resultMonths, resultyears);
+
+    return 0;
+}
